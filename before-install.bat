45cab168a7a994b97eff1e9b6869b244c052482f
Surya Balasubramanian <suryanb@amazon.com>
2014-11-15 01:05:10 +0000

    Sleep for 2000 sec

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,4 @@
 REM Install Internet Information Server (IIS).
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Start-Sleep -s 20
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Start-Sleep -s 2000
