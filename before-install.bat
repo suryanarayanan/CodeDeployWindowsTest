fe1aff3c80be7313f19062fbfc84c8ab93c24a40
Surya Balasubramanian <suryanb@amazon.com>
2014-11-17 21:17:28 +0000

    Remove commands from before install script

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,2 @@
 REM Install Internet Information Server (IIS).
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Start-Sleep -s 2000
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Start-Sleep -s 20
